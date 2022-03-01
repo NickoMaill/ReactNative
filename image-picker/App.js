dba142b4eb25ec55b6768e8c2096ff00220cdb1e
Nicko <nickos@iMac.local>
2022-03-01 16:24:51 +0100

    image picker end

--- image-picker/App.js
+++ image-picker/App.js
@@ -1,12 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import * as ImagePicker from 'expo-image-picker';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+
 
 export default function App() {
-  
+
+  let [selectedImage, setSelectedImage] = useState(null)
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert('Permission to access camera roll is required!');
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setSelectedImage({ localUri: pickerResult.uri });
+  };
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <View>
+          <Image source={{ uri: selectedImage.localUri }}
+            style={{ width: 100, height: 100 }} />
+        </View>
+        <Text>Nicolas Maillols</Text>
+        <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Hello Would you like to see your face ? </Text>
-
+      <View style={styles.viewPic}>
+        <Image source={require("./assets/2753.png")}
+          style={{ width: 100, height: 100 }} />
+      </View>
+      <Text>Nicolas Maillols</Text>
+      <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,5 +60,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  viewPic: {
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderRadius: 60,
+  },
+
+  button: {
+    backgroundColor: 'blue',
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
   },
 });
