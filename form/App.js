b2262bd2fa1e633ded0b7296d6ae341da922b441
Nicko <nickos@iMac.local>
2022-03-02 14:42:56 +0100

    can connect and disconnect

--- form/App.js
+++ form/App.js
@@ -1,35 +1,68 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { View, Text, TouchableOpacity, TextInput, StyleSheet } from "react-native";
 
 export default function Home() {
 
-  const submitForm = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [isLogged, setIsLogged] = useState(false)
 
-  };
+  useEffect(() => {
+    console.log(email.length);
+  }, [email])
+
+  const submitForm = () => {
+    if (email.length >= 6 && password.length >= 6) {
+      setIsLogged(true)
+    }
+  }
+
+  const deconnectUser = () => {
+    setIsLogged(false)
+  }
+
+  if (isLogged !== false) {
+
+    return (
+      <View style={styles.container}>
+
+        <Text style={styles.text}>ACCUEIL</Text>
+
+        <Text>Vous êtes actuellement connecté</Text>
+
+        <TouchableOpacity style={styles.submit} onPress={deconnectUser}>
+          <Text>Se Déconnecter</Text>
+        </TouchableOpacity>
+
+      </View>
+
+    )
+  }
 
   return (
     <View style={styles.container}>
-      <Text
-        style={styles.text}
-      >
-        ACCUEIL
-      </Text>
 
-      <TouchableOpacity onPress={() => navigate("/about")}>
-        <Text>Entrez vos identifiants pour acceder au contenu</Text>
-      </TouchableOpacity>
+      <Text style={styles.text}>ACCUEIL</Text>
+
+      <Text>Entrez vos identifiants pour acceder au contenu</Text>
+
       <TextInput
         style={styles.input}
         placeholder="Email / Pseudo"
+        onChange={(e) => setEmail(e.target.value)}
       />
+
       <TextInput
         style={styles.input}
         placeholder="Mot de passe"
         secureTextEntry
+        onChange={(e) => setPassword(e.target.value)}
       />
+
       <TouchableOpacity style={styles.submit} onPress={submitForm}>
         <Text>Se Connecter</Text>
       </TouchableOpacity>
+
     </View>
   );
 }
